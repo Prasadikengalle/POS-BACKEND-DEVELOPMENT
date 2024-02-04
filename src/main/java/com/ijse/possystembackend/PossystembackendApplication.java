0d3d0ebf73ab17229d930a315f1849ca0616b10a
Prasadikengalle <prasadikengalle@gmail.com>
2024-02-04 18:16:43 +0530

    first commit

---
+++ src/main/java/com/ijse/possystembackend/PossystembackendApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.possystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PossystembackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PossystembackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijse/possystembackend/PossystembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.possystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PossystembackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
